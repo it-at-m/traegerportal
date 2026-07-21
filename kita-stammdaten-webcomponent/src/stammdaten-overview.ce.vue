7b6a87d7a2ecd7781eb21923e5cd5f62de68786f
Max Zollbrecht <m.zollbrecht@muenchen.de>
2026-07-21 08:55:52 +0200

    .

--- kita-stammdaten-webcomponent/src/stammdaten-overview.ce.vue
+++ kita-stammdaten-webcomponent/src/stammdaten-overview.ce.vue
@@ -45,7 +45,7 @@ import { setAccessToken } from "@/util/constants";
 const { loggedIn } = useDBSLoginWebcomponentPlugin(_authChangedCallback);
 
 function _authChangedCallback(authEventDetails?: AuthorizationEventDetails) {
-  console.debug("Auth:" + authEventDetails)
+  console.debug("Auth:" + authEventDetails);
   if (authEventDetails && authEventDetails.accessToken) {
     setAccessToken(authEventDetails.accessToken);
     token.value = authEventDetails.accessToken;
