361720804a75c367d29cee42ea97b26e5a4e12ae
Max Zollbrecht <m.zollbrecht@muenchen.de>
2026-07-23 16:07:14 +0200

    .

--- kita-stammdaten-eai/src/main/java/de/muenchen/rbs/traegerportal/gateway/configuration/GatewayConfig.java
+++ kita-stammdaten-eai/src/main/java/de/muenchen/rbs/traegerportal/gateway/configuration/GatewayConfig.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Slf4j
 @Configuration
