d5807e93c454596b83a087f105bf08a3a7f1ff9f
Max Zollbrecht <m.zollbrecht@muenchen.de>
2026-08-12 09:02:21 +0200

    .

--- kita-stammdaten-webcomponent/src/types/EinrichtungDTO.ts
+++ kita-stammdaten-webcomponent/src/types/EinrichtungDTO.ts
@@ -8,6 +8,53 @@ export class MerkmaleDTO {
   }
 }
 
+export class BetriebserlaubnisDTO {
+  aktuellGueltigeSollbelegung: SollbelegungDTO;
+
+  constructor(aktuellGueltigeSollbelegung: SollbelegungDTO) {
+    this.aktuellGueltigeSollbelegung = aktuellGueltigeSollbelegung;
+  }
+}
+
+export class SollbelegungDTO {
+  plaetzeGesamt: number;
+  plaetzeKinderkrippe: number;
+  plaetzeKindergarten: number;
+  plaetzeHort: number;
+
+  constructor(
+    plaetzeGesamt: number,
+    plaetzeKinderkrippe: number,
+    plaetzeKindergarten: number,
+    plaetzeHort: number
+  ) {
+    this.plaetzeGesamt = plaetzeGesamt;
+    this.plaetzeKinderkrippe = plaetzeKinderkrippe;
+    this.plaetzeKindergarten = plaetzeKindergarten;
+    this.plaetzeHort = plaetzeHort;
+  }
+}
+
+export class SchulsprengelDTO {
+  schulId: number;
+  schule: string;
+
+  constructor(schulId: number, schule: string) {
+    this.schulId = schulId;
+    this.schule = schule;
+  }
+}
+
+export class StadtbezirkDTO {
+  number: number;
+  name: string;
+
+  constructor(number: number, name: string) {
+    this.number = number;
+    this.name = name;
+  }
+}
+
 export class EinrichtungsstatusDTO {
   status: string;
 
@@ -42,6 +89,9 @@ export default class EinrichtungDTO {
   merkmale: MerkmaleDTO;
   kontaktdaten: KontaktdatenDTO;
   aktuellGueltigerStatus: EinrichtungsstatusDTO;
+  aktuellGueltigeBetriebserlaubnis: BetriebserlaubnisDTO;
+  schulsprengel: SchulsprengelDTO;
+  stadtbezirk: StadtbezirkDTO;
 
   constructor(
     id: string,
